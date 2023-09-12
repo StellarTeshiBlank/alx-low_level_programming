5547b424f541a2e23c9a5ad07c9e72baebe52d0a
StellarTeshiBlank <blankteshi079@gmail.com>
2023-09-12 00:53:29 -0700

    My first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
-#include <stdio.h> 
-int main(void) 
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 		puts(str);
