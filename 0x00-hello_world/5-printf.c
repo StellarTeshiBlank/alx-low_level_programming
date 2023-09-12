8657df3fea4b9b9ce2b7f5cdd34119fab0a4fb00
StellarTeshiBlank <blankteshi079@gmail.com>
2023-09-12 01:24:35 -0700

    Printing out a string value

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * return: Always zero (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
