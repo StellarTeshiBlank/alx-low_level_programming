9ada37b9445ab88cc82fd19771667185bacf7ea7
StellarTeshiBlank <blankteshi079@gmail.com>
2023-09-12 01:29:40 -0700

    Working and understanding the C language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - this is the main function
  *
  * return: Always zero (success)
  */
 int main(void)
+	/**
+	 * main - this is the main function
+	 */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
