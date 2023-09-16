88ffa9011bcc0e203993329abeaee2f0a8a97014
StellarTeshiBlank <blankteshi079@gmail.com>
2023-09-16 12:10:19 +0000

    Manipulating variables, if, else and while loop

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,22 +11,21 @@ int main(void)
 {
 	int g, h;
 
-       for (g = 0; g <= 98; g++)
-       {
-	       for (h = g + 1; h <= 99; h++)
-	       {
-		       putchar((g / 10) + '0');
-		       putchar((g % 10) +'0');
-		       putchar(' ');
-		       putchar((h / 10) + '0');
-		       putchar((h % 10) + '0');
-		       if (g == 98 && h == 99)
-		       continue;
-		       putchar(',');
-		       putchar(' ');
-	       }
-       }
-       putchar('\n');
-       return (0);
+	for (g = 0; g <= 98; g++)
+	{
+		for (h = g + 1; h <= 99; h++)
+		{
+			putchar((g / 10) + '0');
+			putchar((g % 10) + '0');
+			putchar(' ');
+			putchar((h / 10) + '0');
+			putchar((h % 10) + '0');
+			if (g == 98 && h == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
