d0fecc3e17208c541b762470a933e4ddb7dfccd6
StellarTeshiBlank <blankteshi079@gmail.com>
2023-09-19 09:32:32 +0000

    Working with nested loops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,7 +15,7 @@ int main(void)
 	fn = afn = 0;
 	while (fn <= 4000000)
 	{
-		fn n1 + n2;
+		fn = n1 + n2;
 		n1 = n2;
 		n2 = fn;
 		if ((n1 % 2) == 0)
@@ -23,6 +23,6 @@ int main(void)
 			afn += n1;
 		}
 	}
-	printf("%d\n", afn);
+	printf("%ld\n", afn);
 	return (0);
 }
