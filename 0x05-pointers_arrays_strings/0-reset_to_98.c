2436bb5463867ee3b5060fffbd6fc1bf4882703b
StellarTeshiBlank <blankteshi079@gmail.com>
2023-09-23 06:42:57 +0000

    Introduction into arrays and Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - resets to 98.
- *
- * Return: Always 0 (Success).
+ * reset_to_98 - takes a pointer to a parameter and updates its value
+ * @n: input integer.
+ * Return: no return.
  */
-void reset_to_98 (int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
