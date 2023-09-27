10c51d325cee20ba1d95e49930c04774949ed89b
StellarTeshiBlank <blankteshi079@gmail.com>
2023-09-27 05:27:30 +0000

    Printing function copies of n bytes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	int r = 0;
+	int i = n;
 
-	for (i = 0; i < n; i++)
-		*(dest + i) = *(src + 1);
-
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
 	return (dest);
 }
